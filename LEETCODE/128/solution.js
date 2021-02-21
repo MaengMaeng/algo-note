e791262933bb4b3fab98fa2f62b1465fe0996f0c
MaengManeg <maybe8189@naver.com>
2021-02-21 15:55:32 +0900

    leetcode 문제 128 주석

--- LEETCODE/128/solution.js
+++ LEETCODE/128/solution.js
@@ -5,8 +5,16 @@
 var longestConsecutive = function(nums) {
     const sortedNums = nums.slice();
     sortedNums.sort((a,b) => a-b);
+    /*
+    *   정렬한다.
+    */
 
     let answer = 0, l = nums.length, n = l ? 1 : 0;
+    /*
+    *   초기값
+    *   l : 배열의 길이
+    *   n : 연속된 수, 배열의 길이가 0이면 0
+    */
 
     for(let i = 1; i < l; i++){
         if(sortedNums[i - 1] + 1 === sortedNums[i]){
@@ -18,6 +26,10 @@ var longestConsecutive = function(nums) {
             n = 1;
         }
     }
+    /*
+    *   연속된 수 찾기
+    */
+
     answer = Math.max(answer, n);
 
     return answer;
