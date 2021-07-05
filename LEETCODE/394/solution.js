82b53c85fcc8d353ebe2d6f61c43b8639724da94
MaengManeg <maybe8189@naver.com>
2021-07-05 19:53:49 +0900

    leetcode 394

---
+++ LEETCODE/394/solution.js
@@ -0,0 +1,38 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var decodeString = function (s) {
+  const stack = [1], str = [""], len = s.length;
+  const digit = /[0-9]/, alpha = /[a-zA-Z]/;
+
+  let index = 0, num = '';
+  for(let i = 0; i < len; i++){
+    // 숫자 일 경우
+    if(digit.test(s[i])){
+      num += s[i];
+    }
+    
+    // 알파벳 일 경우
+    if(alpha.test(s[i])){
+      str[index] += s[i];
+    }
+
+    if(s[i] == "["){
+      stack.push(num);
+      str.push("");
+      index++;
+
+      num = '';
+    }
+
+    if(s[i] == "]"){
+      str[index - 1] += str.pop().repeat(stack.pop());
+      index--;
+    }
+  }
+
+  return str[0];
+};
+
+console.log(decodeString("20[abc]"));
