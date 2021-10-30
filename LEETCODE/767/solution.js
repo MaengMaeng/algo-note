b0bc6d2ba332f8885923f8cc90fff597517ec60a
MaengManeg <maybe8189@naver.com>
2021-10-30 18:29:13 +0900

    leetcode 767

---
+++ LEETCODE/767/solution.js
@@ -0,0 +1,48 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+ var reorganizeString = function(s) {
+  const map = {};
+
+  for(let c of s){
+    if(!map[c]) map[c] = 0;
+    map[c]++;
+  }
+
+  const keys = Object.keys(map);
+  const mid = s.length % 2 == 0 ? (s.length / 2) : ((s.length + 1) / 2);
+  const chars = [];
+
+  for(let key of keys){
+    if(map[key] > mid) return "";
+    chars.push([key, map[key]]);
+  }
+
+  let answer = "";
+
+  const dfs = (current, str, chars) => {
+    if(answer.length > 0) return;
+
+    if(str.length == s.length){
+      answer = str;
+      return;
+    }
+
+    // 제일 많이 있는 순으로 정렬
+    chars.sort((a, b) => b[1] - a[1]);
+
+    for(let i = 0; i < chars.length; i++){
+      // 다른 문자면
+      if(current != chars[i][0]){
+        chars[i][1]--;
+        dfs(chars[i][0], str + chars[i][0], chars);
+        chars[i][1]++;
+      }
+    }
+  }
+
+  dfs("", "", chars);
+
+  return answer;
+};
